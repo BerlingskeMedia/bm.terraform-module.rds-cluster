a720704abb2242395557bd46dc2df7ea4e323b0f
Maciej Gierwatowski <maciej.gierwatowski@gmail.com>
2020-04-30 17:02:11 +0200

    mx_tools

--- main.tf
+++ main.tf
@@ -1,30 +1,76 @@
+
+module "label" {
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  namespace  = var.namespace
+  name       = var.name
+  stage      = var.stage
+  delimiter  = var.delimiter
+  attributes = var.attributes
+  tags       = var.tags
+}
+
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+module "endpoint" {
+  source = "git@github.com:BerlingskeMedia/bm.terraform-module.secrets?ref=mx_tools"
+  //source = "../bm.terraform-module.secrets"
+  enabled        = var.enabled
+  namespace      = var.namespace
+  stage          = var.stage
+  name           = var.name
+  tags           = var.tags
+  labeled_path   = true
+  var_name       = "rds_endpoint"
+  parameter_type = "String"
+  value          = module.rds_cluster_aurora_mysql.endpoint
+}
+
+module "rds_pass" {
+  source = "git@github.com:BerlingskeMedia/bm.terraform-module.secrets?ref=mx_tools"
+  //source = "../bm.terraform-module.secrets"
+  namespace       = var.namespace
+  stage           = var.stage
+  name            = var.name
+  tags            = var.tags
+  enabled         = var.enabled && var.master_password == ""
+  parameter_type  = "SecureString"
+  kms_encrypt     = true
+  generate_secret = true
+  var_name        = "rds_password"
+  labeled_path    = true
+}
+
+
+
+resource "aws_security_group" "rds_sg" {
+  count       = var.enabled ? 1 : 0
+  name        = "${module.label.id}-in"
+  description = "Security group allows to access RDS ${module.label.id}"
+  vpc_id      = var.vpc_id
+
+  tags = merge(var.tags, { "Name" = module.label.id })
+}
+
 module "rds_cluster_aurora_mysql" {
   source         = "git::https://github.com/cloudposse/terraform-aws-rds-cluster.git?ref=tags/0.21.0"
   namespace      = var.namespace
   stage          = var.stage
-  name           = "${var.name}-rds"
-  engine         = var.engine
+  name           = "${var.name}"
+  engine         = var.db_engine
   engine_mode    = "provisioned"
-  cluster_family = var.cluster_family
-  cluster_size   = var.cluster_size
-  admin_user     = var.master_user_name
+  cluster_family = var.db_cluster_family
+  cluster_size   = var.db_cluster_size
+  admin_user     = var.db_root_user
   //admin_password  = data.aws_kms_secrets.rds.plaintext["master_password"]
-  admin_password  = var.master_password
-  db_name         = var.db_name
+  admin_password  = module.rds_pass.value
+  db_name         = var.dbname
   db_port         = var.rds_port
-  instance_type   = var.instance_type
+  instance_type   = "db.t3.small"
   vpc_id          = var.vpc_id
-  security_groups = var.security_groups
+  security_groups = aws_security_group.rds_sg.*.id
   subnets         = var.subnets
   enabled         = var.enabled
+  attributes      = var.attributes
 }
-
-resource "aws_ssm_parameter" "secret" {
-  count       = var.enabled ? 1 : 0
-  name        = "/${var.namespace}/${var.stage}/${var.name}/rds_endpoint"
-  description = "RDS endpoint url"
-  type        = "String"
-  value       = module.rds_cluster_aurora_mysql.endpoint
-  tags        = var.tags
-}
-
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,39 @@
 output "rds_endpoint" {
   description = "Endpoint url of created RDS"
-  value       = module.rds_cluster_aurora_mysql.endpoint
+  value       = module.endpoint.value
+}
+
+output "rds_sg_id" {
+  description = "Security Group ID allows to connect to RDS"
+  value       = join("", aws_security_group.rds_sg.*.id)
+}
+
+output "ssm_arns" {
+  value = concat(module.endpoint.ssm_arn, module.rds_pass.ssm_arn)
+}
+
+output "kms_arns" {
+  value = concat(module.rds_pass.kms_arn)
+}
+
+output "dbuser_arns" {
+  value = [for db_id in module.rds_cluster_aurora_mysql.dbi_resource_ids :
+    "arn:aws:rds-db:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:dbuser:${db_id}/${var.db_root_user}"
+  ]
+}
+
+output "root_username" {
+  value = var.db_root_user
+}
+
+output "password_ssm_arn" {
+  value = module.rds_pass.ssm_arn
+}
+
+output "endpoind_ssm_arn" {
+  value = module.endpoint.ssm_arn
+}
+
+output "db_name" {
+  value = var.dbname
 }
